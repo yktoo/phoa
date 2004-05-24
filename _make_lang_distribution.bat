3c81fd95fda15efe7cbc9170949bb08e893e0a35
dale <dale@6a04f9ad-7319-4938-9663-66443cc0e230>
2004-05-24 05:30:37 +0000

    Исправлено создание языкового пакета

--- _make_lang_distribution.bat
+++ _make_lang_distribution.bat
@@ -1,6 +1,6 @@
 @echo off
 rem ********************************************************************************************************************
-rem $Id: _make_lang_distribution.bat,v 1.2 2004-04-15 12:54:10 dale Exp $
+rem $Id: _make_lang_distribution.bat,v 1.3 2004-05-24 05:30:37 dale Exp $
 rem --------------------------------------------------------------------------------------------------------------------
 rem PhoA image arranging and searching tool
 rem Copyright 2002-2004 Dmitry Kann, http://phoa.narod.ru
@@ -11,4 +11,4 @@ if exist IS-install\phoa-lang-src.zip del IS-install\phoa-lang-src.zip
 
 rem -m3    = compression normal
 rem -afzip = create zip archive
-start /w C:\Progra~1\WinRAR\WinRAR.exe a -m3 -afzip IS-install\phoa-lang-src.zip *.dtls ..\dttm\dttm.exe Lang-distribution-ReadMe.txt IS-install\phoa.iss IS-install\eula-eng.rtf IS-install\eula-rus.rtf
+start /w C:\Progra~1\WinRAR\WinRAR.exe a -m3 -afzip IS-install\phoa-lang-src.zip *.xdtls ..\dttm\dttm.exe Lang-distribution-ReadMe.txt IS-install\SetupMessages.txt IS-install\eula-eng.rtf IS-install\eula-rus.rtf
